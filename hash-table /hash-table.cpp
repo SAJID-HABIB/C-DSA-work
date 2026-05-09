7f058bab71899c0500099b05ee040b05e1fc63ef
Sajid Habib <sajidchughtai779@gmail.com>
2026-05-09 14:41:04 +0500

    Hash table in the DSA C++

---
+++ hash-table /hash-table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+
+    unordered_map<int, string> student;
+
+    student[101] = "Ali";
+    student[102] = "Ahmed";
+    student[103] = "Sara";
+
+    cout << student[103];
+
+}
+
+
+// es ko exame point of view sy prepare krna ha codding ni ha zeyzda srif conceptula question aee gy ?
